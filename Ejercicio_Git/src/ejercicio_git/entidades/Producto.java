857e140f82eb590e455b73fdafe11face3ff5565
Sofia Barbeito <sofiabarbeito@gmail.com>
2020-09-10 21:11:55 -0300

    primer commit clase producto

---
+++ Ejercicio_Git/src/ejercicio_git/entidades/Producto.java
@@ -0,0 +1,26 @@
+
+package ejercicio_git.entidades;
+
+public class Producto {
+
+    private int id_producto;
+    private String nombre;
+    private int precio;
+    private String proveedor;
+    private int cantidad;
+
+    public void Asignacion(int id_producto, String nombre, int precio, String proveedor, int cantidad) {
+        this.id_producto = id_producto;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.proveedor = proveedor;
+        this.cantidad = cantidad;
+    }
+    
+
+    public void Retorno(){
+        
+        
+    }
+    
+}
